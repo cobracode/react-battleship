283c13d1057751bfb6f2cde10b77189a4a09e263
Nader Matinnia <cobracode@users.noreply.github.com>
2025-06-24 14:39:11 -0700

    board skeleton

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,13 @@
+import './Board.css';
+
+const Board = ({ squares }: { squares: string[] }) => {
+  return <div className="board">
+    {squares.map((square, index) => (
+      <div key={index} className="square">
+        {square}
+      </div>
+    ))}
+  </div>;
+};
+
+export default Board;
---
+++ src/components/GameStatus/GameStatus.tsx
@@ -0,0 +1,5 @@
+const GameStatus = () => {
+  return <div>GameStatus</div>;
+};
+
+export default GameStatus;
